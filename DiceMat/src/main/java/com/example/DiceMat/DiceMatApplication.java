47c5bf8965fa870dcf92d0b8178ed888a20b4c00
mateusbarbosa105-web <mateus.barbosa.105@ufrn.edu.br>
2025-11-21 17:12:31 -0300

    Aquivos spring boot adicionados
    base para futuros commits

---
+++ DiceMat/src/main/java/com/example/DiceMat/DiceMatApplication.java
@@ -0,0 +1,13 @@
+package com.example.DiceMat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiceMatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiceMatApplication.class, args);
+	}
+
+}
---
+++ DiceMat/src/test/java/com/example/DiceMat/DiceMatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DiceMat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiceMatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
